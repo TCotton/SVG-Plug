1e33e8d5225d3fc1b8323e041a07bd14181fd4e2
Andy Walpole <andy@suburban-glory.com>
2011-11-25 05:58:51 +0000

    new version

--- svgplug-client.js
+++ svgplug-client.js
@@ -1,36 +1,36 @@
 var SVG = {};
 
 SVG.ImgReplace = (function () {
-    //private attributes
-    var _objX, _jsonData, _y, _len, _key, _bodyChildren, _x, _svgObject, _newImg, _root, _newParent, _result;
+    //attributes
+    var objX, jsonData, y, len, key, bodyChildren, x, svgObject, newImg, root, newParent, result;
 
-    // private methods
+    //methods
 
-    function _getJSON() {
+    function getJSON() {
         // getJSON uses standard AJAX techniques to snatch and parse JSON file created by service side script
-        _objX = new XMLHttpRequest();
+        objX = new XMLHttpRequest();
 
-        if (_objX !== null) {
+        if (objX !== null) {
 
-            _objX.open("GET", "json/low-resulution-images.json", true);
+            objX.open("GET", "json/low-resulution-images.json", true);
 
-            _objX.onreadystatechange = function () {
+            objX.onreadystatechange = function () {
 
                 //if readyState is not 4 or or status not 200 then there is a problem that needs attending
-                if (_objX.readyState === 4) {
-                    if (_objX.status === 200) {
+                if (objX.readyState === 4) {
+                    if (objX.status === 200) {
 
-                        _jsonData = eval("(" + _objX.responseText + ")"); //take result as an JavaScript object
-                        _processJSON();
+                        jsonData = eval("(" + objX.responseText + ")"); //take result as an JavaScript object
+                        processJSON();
 
                     } else {
 
-                        alert('HTTP error ' + _objX.status);
+                        alert('HTTP error ' + objX.status);
                     }
                 }
             };
 
-            _objX.send();
+            objX.send();
 
         } else {
 
@@ -38,81 +38,85 @@ SVG.ImgReplace = (function () {
         }
     } // end getJSON
 
-    function _processJSON() {
+    function processJSON() {
 
-        _bodyChildren = document.body.getElementsByTagName("object");
+        bodyChildren = document.body.getElementsByTagName("object");
 
-        _x = 0;
+        x = 0;
 
-        for (_len = _bodyChildren.length; _x < _len; _x += 1) {
+        for (len = bodyChildren.length; x < len; x += 1) {
             // Look through all objects on the page
-            _svgObject = _bodyChildren[_x];
+            svgObject = bodyChildren[x];
 
-            _newImg = document.createElement("img");
+            // make sure object is SVG
+            if (svgObject.getAttribute("type") === "image/svg+xml") {
 
-            _result = _bodyChildren[_x].data.substr(0, _bodyChildren[_x].data.lastIndexOf('.'));
-            // Removes the file extension from SVG url
-            _result = _result.substring(_result.lastIndexOf('/') + 1);
-            // Removes everything before the last slash
-            for (_key in _jsonData) {
-                // loop through JSON object
-                if (_jsonData.hasOwnProperty(_key)) {
+                newImg = document.createElement("img");
 
-                    if (_result === _jsonData[_key].file.substr(0, _jsonData[_key].file.lastIndexOf('.'))) {
+                result = bodyChildren[x].data.substr(0, bodyChildren[x].data.lastIndexOf('.'));
+                // Removes the file extension from SVG url
+                result = result.substring(result.lastIndexOf('/') + 1);
+                // Removes everything before the last slash
+                for (key in jsonData) {
+                    // loop through JSON object
+                    if (jsonData.hasOwnProperty(key)) {
 
-                        _newImg.src = "images/l/" + _jsonData[_key].file;
+                        if (result === jsonData[key].file.substr(0, jsonData[key].file.lastIndexOf('.'))) {
 
+                            newImg.src = "images/l/" + jsonData[key].file;
+
+                        } // end if
                     } // end if
-                } // end if
-            } // end for in loop
-            // Apply width and height of the svg to the new img if that is set in svg object
-            if (_svgObject.getAttribute("width") !== null) {
+                } // end for in loop
+                // Apply width and height of the svg to the new img if that is set in svg object
+                if (svgObject.getAttribute("width") !== null) {
 
-                _newImg.width = _svgObject.getAttribute("width");
-                _newImg.height = _svgObject.getAttribute("height");
+                    newImg.width = svgObject.getAttribute("width");
+                    newImg.height = svgObject.getAttribute("height");
 
-            }
+                }
 
-            // If object has id then use that attribute
-            if (_svgObject.getAttribute("id") !== null) {
+                // If object has id then use that attribute
+                if (svgObject.getAttribute("id") !== null) {
 
-                _newImg.id = _svgObject.getAttribute("id");
+                    newImg.id = svgObject.getAttribute("id");
 
-            }
+                }
 
-            // alt for image must be placed in title attribute
-            if (_svgObject.getAttribute("title") !== null) {
+                // alt for image must be placed in title attribute
+                if (svgObject.getAttribute("title") !== null) {
 
-                _newImg.alt = _svgObject.getAttribute("title");
+                    newImg.alt = svgObject.getAttribute("title");
 
-            }
+                }
 
-            // apply classname if known
-            if (_svgObject.getAttribute("class") || _svgObject.getAttribute("className") !== null) {
+                // apply classname if known
+                if (svgObject.getAttribute("class") || svgObject.getAttribute("className") !== null) {
 
-                _newImg.className = _svgObject.getAttribute("class") || _svgObject.getAttribute("className");
+                    newImg.className = svgObject.getAttribute("class") || svgObject.getAttribute("className");
 
-            }
+                }
 
-            // declare parent of svg object
-            _root = _svgObject.parentNode;
-            _newParent = document.createElement('div');
+                // declare parent of svg object
+                root = svgObject.parentNode;
+                newParent = document.createElement('div');
 
-            // Insert new node as a child or the root directly before the svg object
-            _root.insertBefore(_newParent, _svgObject);
+                // Insert new node as a child or the root directly before the svg object
+                root.insertBefore(newParent, svgObject);
 
-            // Append svg object to the new parent
-            _newParent.appendChild(_svgObject);
+                // Append svg object to the new parent
+                newParent.appendChild(svgObject);
 
-            // Append new image to the parent node
-            _svgObject.parentNode.appendChild(_newImg);
+                // Append new image to the parent node
+                svgObject.parentNode.appendChild(newImg);
 
-            // Remove the svg object
-            //$svgObject.parentNode.removeChild($svgObject);
-            // ouch, removing the node has given me headaches 
-            // so for now we will just set it to display none
-            _svgObject.className += " hidden";
+                // Remove the svg object
+                //$svgObject.parentNode.removeChild($svgObject);
+                // ouch, removing the node has given me headaches 
+                // so for now we will just set it to display none
+                svgObject.className += " hidden";
 
+            } // end if statement
         } // end for loop
     } // end processJSON
     return {
@@ -123,7 +127,7 @@ SVG.ImgReplace = (function () {
             // don't forget to change for intenet explorer
             if (!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1")) {
 
-                _getJSON();
+                getJSON();
 
             }
         }
